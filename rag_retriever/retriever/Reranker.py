cf41c0225fd8aba733976c64085d37002903a925
arkanto99 <pablorodriguez.fernandez@usc.gal>
2025-08-13 12:26:38 +0200

    [ADD] Qwen3 Reranker

--- rag_retriever/retriever/Reranker.py
+++ rag_retriever/retriever/Reranker.py
@@ -2,6 +2,8 @@ from FlagEmbedding import FlagReranker
 from typing import List, Any
 from sentence_transformers import SentenceTransformer
 import numpy as np
+from transformers import AutoModel, AutoTokenizer, AutoModelForCausalLM
+import torch
 
 class SentenceTransformerReranker(SentenceTransformer):
     def __init__(self, model_name: str, cache_dir: str = None, use_fp16: bool = True):
@@ -41,6 +43,63 @@ class FlagEmbeddingReranker(FlagReranker):
         scores = self.compute_score(pairs, normalize=normalize) #Using FlagEmbedding's compute_score method
         return scores
 
+class Qwen3Reranker:
+    def __init__(self, model_name: str, cache_dir: str = None, use_fp16: bool = True):
+        #Official code adapted from https://github.com/QwenLM/Qwen3-Embedding
+        """
+        Args:
+            model_name: Name of the Qwen3 model to use
+            cache_dir: Directory to cache the model files
+            use_fp16: Whether to use half-precision for inference
+        """
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir, padding_side='left')
+        self.model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=cache_dir, torch_dtype='auto', device_map='auto').eval()
+        self.token_false_id = self.tokenizer.convert_tokens_to_ids("no")
+        self.token_true_id = self.tokenizer.convert_tokens_to_ids("yes")
+        self.max_length = 8192
+        
+        prefix = "<|im_start|>system\nJudge whether the Document meets the requirements based on the Query and the Instruct provided. Note that the answer can only be \"yes\" or \"no\".<|im_end|>\n<|im_start|>user\n"
+        suffix = "<|im_end|>\n<|im_start|>assistant\n<think>\n\n</think>\n\n"
+        self.prefix_tokens = self.tokenizer.encode(prefix, add_special_tokens=False)
+        self.suffix_tokens = self.tokenizer.encode(suffix, add_special_tokens=False)
+        
+        self.task = 'Given a web search query, retrieve relevant passages that answer the query'
+
+    def format_instruction(self, instruction, query, doc):
+        if instruction is None:
+            instruction = 'Given a web search query, retrieve relevant passages that answer the query'
+        output = "<Instruct>: {instruction}\n<Query>: {query}\n<Document>: {doc}".format(instruction=instruction,query=query, doc=doc)
+        return output
+
+    def process_inputs(self, pairs):
+        inputs = self.tokenizer(
+            pairs, padding=False, truncation='longest_first',
+            return_attention_mask=False, max_length=self.max_length - len(self.prefix_tokens) - len(self.suffix_tokens)
+        )
+        for i, ele in enumerate(inputs['input_ids']):
+            inputs['input_ids'][i] = self.prefix_tokens + ele + self.suffix_tokens
+        inputs = self.tokenizer.pad(inputs, padding=True, return_tensors="pt")
+        for key in inputs:
+            inputs[key] = inputs[key].to(self.model.device)
+        return inputs
+
+    @torch.no_grad()
+    def compute_logits(self, inputs, **kwargs):
+        batch_scores = self.model(**inputs).logits[:, -1, :]
+        true_vector = batch_scores[:, self.token_true_id]
+        false_vector = batch_scores[:, self.token_false_id]
+        batch_scores = torch.stack([false_vector, true_vector], dim=1)
+        batch_scores = torch.nn.functional.log_softmax(batch_scores, dim=1)
+        scores = batch_scores[:, 1].exp().tolist()
+        return scores
+
+    def compute_scores(self, query: str, passages: List[str], normalize) -> List[float]:
+        pairs = [self.format_instruction(self.task, query, passage) for passage in passages]
+        inputs = self.process_inputs(pairs)
+        scores = self.compute_logits(inputs)
+        if normalize:
+            scores = [1 / (1 + np.exp(-score)) for score in scores]  # Sigmoid normalization
+        return scores
 
 class Reranker:
     def __init__(self, model_name, hf_cache_dir, use_fp16=True, normalize=True):
@@ -56,6 +115,9 @@ class Reranker:
         if model_name in ["BAAI/bge-reranker-v2-m3"]:
             # Use FlagEmbedding for BGE reranker
             self.reranker = FlagEmbeddingReranker(model_name, cache_dir=hf_cache_dir, use_fp16=self.use_fp16)
+        elif model_name.startswith("Qwen"):
+            # Use Qwen3 reranker
+            self.reranker = Qwen3Reranker(model_name, cache_dir=hf_cache_dir, use_fp16=self.use_fp16)
         else:
             # Use SentenceTransformer for other models
             self.reranker = SentenceTransformerReranker(model_name, cache_dir=hf_cache_dir, use_fp16=self.use_fp16)
