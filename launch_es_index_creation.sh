baba6d503fa85a4bc0b2907c95900f31557c0255
arkanto99 <pablorodriguez.fernandez@usc.gal>
2025-09-30 10:46:53 +0200

    test

--- launch_es_index_creation.sh
+++ launch_es_index_creation.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
  python3 elasticsearch/create_elastic_database_with_embeddings.py \
      --index "/home/compartido/pabloF/nos-rag-eval/elasticsearch/index_embedding_example.json" \
      --hf_cache_dir "/home/compartido/pabloF/cache" \
